2f64331cf49eb200474ec94bbf0c79674dc07ec2
Zill Ding <zilldzy@gmail.com>
2024-05-13 23:09:23 +0800

    Improve UI layout to adopt responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -39,53 +39,56 @@ export default function Home() {
   }, [input]);
 
   return (
-    <main className="flex min-h-screen flex-col gap-4 p-4">
-      <div className="relative">
-        <h1 className="mb-2">
-          <strong>SVG to URL app</strong>: convert SVG string to data URL
-        </h1>
-        <textarea
-          className="block w-full border bg-transparent"
-          placeholder="<svg>...</svg>"
-          disabled={loading}
-          value={input}
-          onChange={(e) => {
-            setInput(e.target.value.trim());
-          }}
-        />
-        <button
-          className="absolute bottom-2 right-2 border"
-          onClick={() => {
-            setInput("");
-          }}
-        >
-          clear
-        </button>
-      </div>
-      {loading ? (
-        <p className="text-center">processing...</p>
-      ) : (
-        input &&
-        (error ? (
-          <pre className="text-wrap text-center text-red-600">
-            {(error as Error).toString()}
-          </pre>
+    <main className="flex min-h-screen flex-col gap-2 p-4">
+      <h1>
+        <strong>SVG to URL app</strong>: convert SVG string to data URL
+      </h1>
+      <div className="flex flex-grow flex-col gap-4 md:flex-row">
+        <div className="relative flex flex-col md:w-1/2">
+          <textarea
+            className="block w-full flex-grow border bg-transparent"
+            placeholder="<svg>...</svg>"
+            disabled={loading}
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value.trim());
+            }}
+          />
+          <button
+            className="absolute bottom-2 right-2 border"
+            onClick={() => {
+              setInput("");
+            }}
+          >
+            clear
+          </button>
+        </div>
+        {loading ? (
+          <p className="flex items-center justify-center text-wrap text-center md:w-1/2">
+            processing...
+          </p>
         ) : (
-          !!url && (
-            <div>
-              <p>Result:</p>
-              <input
-                className="mb-4 w-full border bg-transparent"
-                readOnly
-                value={url}
-              />
-              <div className="relative min-h-40">
-                <Image className="dark:invert" alt="preview" fill src={url} />
+          input &&
+          (error ? (
+            <pre className="flex items-center justify-center text-wrap text-center text-red-600 md:w-1/2">
+              {(error as Error).toString()}
+            </pre>
+          ) : (
+            !!url && (
+              <div className="flex flex-grow flex-col">
+                <input
+                  className="mb-4 w-full border bg-transparent"
+                  readOnly
+                  value={url}
+                />
+                <div className="relative min-h-40 items-center justify-center md:flex-grow">
+                  <Image className="dark:invert" alt="preview" fill src={url} />
+                </div>
               </div>
-            </div>
-          )
-        ))
-      )}
+            )
+          ))
+        )}
+      </div>
     </main>
   );
 }
